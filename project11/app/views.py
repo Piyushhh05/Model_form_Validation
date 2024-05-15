2a0e109b4ec8319c336cb02e8cc0371c21ad53bf
Piyushhh <168396994+Piyushhh05@users.noreply.github.com>
2024-05-15 12:06:32 +0530

    Add files via upload

---
+++ project11/app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from app.forms import *
+from django.http import HttpResponse
+
+# Create your views here.
+
+def register(request):
+    ECFO = CredForm()
+    d = {'ECFO':ECFO}
+    if request.method == 'POST' and request.FILES:
+        CFDO = CredForm(request.POST, request.FILES)
+        if CFDO.is_valid():
+            CFDO.save()
+            return HttpResponse('Done')
+        return HttpResponse('invalid Data')
+    return render(request, 'register.html', d)
